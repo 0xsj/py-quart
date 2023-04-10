beed01ce7ce6f8fdae818e686fefc63094ae5ddb
sjtommylee <sjtommylee@gmail.com>
2023-04-09 19:49:16 -0700

    chore: commit save point

--- conduit/__init__.py
+++ conduit/__init__.py
@@ -1,22 +1,31 @@
-from quart import Quart, Blueprint
+from quart import Quart, Blueprint, render_template, websocket
 from quart_schema import QuartSchema
 
 app = Quart(__name__)
 
 
-async def app(scope, receive, send):
-    if scope["type"] != "http":
-        raise Exception("Only the HTTP protocol is supported")
+@app.route("/")
+async def hello():
+    return await render_template("test.html")
 
-    await send({
-        'type': 'http.response.start',
-        'status': 200,
-        'headers': [
-            (b'content-type', b'text/plain'),
-            (b'content-length', b'5'),
-        ],
-    })
-    await send({
-        'type': 'http.response.body',
-        'body': b'hello',
-    })
+
+@app.route("/api")
+async def json():
+    return {"foo": "baz"}
+
+
+@app.websocket("/ws")
+async def ws():
+    while True:
+        await websocket.send("Foo")
+        await websocket.send({"fooz": "baz"})
+
+
+@app.before_serving
+async def startup():
+    print("startup func")
+
+
+@app.after_serving
+async def shutdown():
+    print("shutting down...")
