a63118979b9c4e577be1c863a4f07ef2bc48e3a9
sjtommylee <sjtommylee@gmail.com>
2023-04-09 18:24:12 -0700

    chore: commit save point

--- conduit/__init__.py
+++ conduit/__init__.py
@@ -1,5 +1,22 @@
-import psycopg2
-from quart import Quart, Blueprint
-from quart_schema import QuartSchema
+# import psycopg2
+# from quart import Quart, Blueprint
+# from quart_schema import QuartSchema
 
-app = Quart(__name__)
+# app = Quart(__name__)
+
+async def app(scope, receive, send):
+    if scope["type"] != "http":
+        raise Exception("Only the HTTP protocol is supported")
+
+    await send({
+        'type': 'http.response.start',
+        'status': 200,
+        'headers': [
+            (b'content-type', b'text/plain'),
+            (b'content-length', b'5'),
+        ],
+    })
+    await send({
+        'type': 'http.response.body',
+        'body': b'hello',
+    })
