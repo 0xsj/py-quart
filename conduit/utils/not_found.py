aa14d88e68959171cdb067a7bdaa3a4cda52fb8c
sjtommylee <sjtommylee@gmail.com>
2023-04-11 17:03:31 -0700

    chore: commit save point

--- conduit/utils/not_found.py
+++ conduit/utils/not_found.py
@@ -1,2 +1,14 @@
+from typing import Type
+
+
 class NotFound(Exception):
-    pass
+    def __init__(self, message: str = "Not Found"):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self) -> str:
+        return f"{self.__class__.__name__}: {self.message}"
+
+    @property
+    def error_type(self) -> Type[Exception]:
+        return self.__class__
